0e72598c21e4640793afeef4b5a79168339b5a13
thgcunha <thiago290106@gmail.com>
2024-09-13 17:19:27 -0300

    fechando atividade

--- js/script.js
+++ js/script.js
@@ -24,3 +24,20 @@ function calcAreaTrapezio() {
     let area =  (basemaior + basemenor)  * alturatrapezio  / 2  
     document.getElementById("areatrapezio").innerText = area
 }
+
+
+function calcEquacaoSegundoGrau() {
+    let valora = document.getElementById("valora").value
+    let valorb = document.getElementById("valorb").value
+    let valorc = document.getElementById("valorc").value
+    let delta =  valorb**2 - 4* valora * valorc
+    document.getElementById("delta").innerText ="O valor de delta é:" + delta
+
+    let x1 = (-valorb - Math.sqrt(delta)) /( 2 * valora)
+    document.getElementById("x1").innerText = "O valor de x1 é:" +  x1
+
+    let x2= (-valorb + Math.sqrt(delta)) / (2 *valora)
+    document.getElementById("x2").innerText = "O valor de x1 é:" +  x2
+    
+}
+
